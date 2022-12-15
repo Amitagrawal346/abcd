edfb18c187a7d1fb1aca420980b8a234818b4406
amit@gmail.com <you@example.com>
2022-12-15 18:44:14 +0530

    hdgd

---
+++ handler.js
@@ -0,0 +1,33 @@
+const AWS = require('aws-sdk');
+const db = new AWS.DynamoDB.DocumentClient();
+const uuid = require('uuid/v4');
+
+const postsTable = process.env.POSTS_TABLE;
+// Create a response
+function response(statusCode, message) {
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify(message)
+  };
+}
+
+module.exports.createPost = (event, context, callback) => {
+  const reqBody = JSON.parse(event.body);
+  const post = {
+    id: uuid(),
+    createdAt: new Date().toISOString(),
+    userId: 1,
+    title: reqBody.title,
+    body: reqBody.body
+  };
+  return db
+    .put({
+      TableName: postsTable,
+      Item: post
+    })
+    .promise()
+    .then(() => {
+      callback(null, response(201, post));
+    })
+    .catch((err) => response(null, response(err.statusCode, err)));
+};
